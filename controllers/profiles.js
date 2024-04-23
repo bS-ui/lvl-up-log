25c70ba84b19ea694c37640b4e90c37f6af8dd67
bS-ui <brandonxsears@gmail.com>
2024-04-23 15:55:28 -0400

    Adds functionality for adding a recently played game

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -59,7 +59,6 @@ function setFav(req, res) {
 }
 
 function newFav(req, res) {
-  console.log(req.body)
   Profile.findById(req.params.profileId)
   .then(profile => {
     profile.favoriteGame = req.body
@@ -76,9 +75,68 @@ function newFav(req, res) {
       })
   })
 }
+
+function setRpg(req, res) {
+  fetch('https://api.rawg.io/api/genres?page_size=40&key=b658202e9c5c4018973e0e3ce9a98977')
+  .then(response => response.json())
+  .then(genres => {
+    Profile.findById(req.params.profileId)
+    .then(profile => {
+      if (req.body.genre == undefined) {
+        fetch(`https://api.rawg.io/api/games?genres=action&key=b658202e9c5c4018973e0e3ce9a98977`)
+        .then(response => response.json())
+        .then(games => {
+        res.render('profiles/setrecentlyplayedgame', {
+          selectedGenre: req.body.genre,
+          games,
+          genres,
+          profile,
+          title: `lvlUpLog: ${profile.name}`
+        })
+      })
+      } else {
+        fetch(`https://api.rawg.io/api/games?genres=${req.body.genre == 'RPG' ? 'role-playing-games-rpg' : req.body.genre == 'Board Games' ? 'board-games' : req.body.genre == 'Massively Multiplayer' ? 'massively-multiplayer' : (req.body.genre).split(" ").join("").toLowerCase()}&key=b658202e9c5c4018973e0e3ce9a98977`)
+        .then(response => response.json())
+        .then(games => {
+        res.render('profiles/setrecentlyplayedgame', {
+          selectedGenre: req.body.genre,
+          games,
+          genres,
+          profile,
+          title: `lvlUpLog: ${profile.name}`
+        })
+      })
+      }
+    })
+  })
+  .catch(error => {
+    console.error('Error:', error)
+  })
+}
+
+function newRpg(req, res) {
+  Profile.findById(req.params.profileId)
+  .then(profile => {
+    profile.recentlyPlayedGames.push(req.body)
+    profile.recentlyPlayedGames[profile.recentlyPlayedGames.length-1].review = req.body
+    profile.save()
+    .then
+      fetch(`https://api.rawg.io/api/games/${(profile.recentlyPlayedGames[profile.recentlyPlayedGames.length-1].name).replace(/\s+/g, '-').toLowerCase()}?key=b658202e9c5c4018973e0e3ce9a98977`)
+      .then(response => response.json())
+      .then(game => {
+        profile.recentlyPlayedGames[profile.recentlyPlayedGames.length-1].imageUrl = game.background_image
+        profile.save()
+        .then
+          res.redirect(`/profiles/${profile._id}`)
+      })
+  })
+}
+
 export {
   index,
   show,
   setFav,
   newFav,
+  setRpg,
+  newRpg,
 }
--- routes/profiles.js
+++ routes/profiles.js
@@ -13,11 +13,21 @@ router.get('/:profileId', profilesCtrl.show)
 // GET /profiles/:profileId/set-fav
 router.get('/:profileId/set-fav', profilesCtrl.setFav)
 
+// GET /profiles/:profileId/set-rpg
+router.get('/:profileId/set-rpg', profilesCtrl.setRpg)
+
 // POST /profiles/:profileID/set-fav
 router.post('/:profileId/set-fav', profilesCtrl.setFav)
 
+// POST /profiles/:profileID/set-rpg
+router.post('/:profileId/set-rpg', profilesCtrl.setRpg)
+
 // POST /profiles/:profileID/new-fav
 router.post('/:profileId/new-fav', profilesCtrl.newFav)
+
+// POST /profiles/:profileID/new-rpg
+router.post('/:profileId/new-rpg', profilesCtrl.newRpg)
+
 export {
   router
 }
